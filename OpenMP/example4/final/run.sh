9ab5690da71f10addf476643fa16705e6d259a1a
Taylor Barnes <tbarnes1.vt.edu>
2019-08-05 21:46:56 -0400

    Update OpenMP submission scripts

--- OpenMP/example4/final/run.sh
+++ OpenMP/example4/final/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=example4
 #SBATCH --output=sbatch.out
-#SBATCH --ntasks-per-node=24
+#SBATCH --ntasks-per-node=40
 #SBATCH --nodes=1
-#SBATCH -p molssi
+#SBATCH --time=00:05:00
+#SBATCH -p molssi-40core
 
 source /gpfs/projects/molssi/modules-gnu
 export OMP_NUM_THREADS=4
