d328b6bd9ebd11a6edb394d001625f72da5149e6
Dmitry I. Lyakh <quant4me@gmail.com>
2021-02-24 21:01:34 -0500

    Updated license in CUDA/BLAS_Lib_CUDA to BSD-3.

--- CUDA/BLA_Lib_CUDA/README.md
+++ CUDA/BLA_Lib_CUDA/README.md
@@ -1,11 +1,11 @@
-CUDA Tutorial: Basic Linear Algebra (BLA) Library
+CUDA Tutorial: Basic Linear Algebra (BLA) Library Exercise
 
 AUTHOR: Dmitry I. Lyakh (Liakh): quant4me@gmail.com, liakhdi@ornl.gov
 
 Copyright (C) 2018-2018 Dmitry I. Lyakh (Liakh)
 Copyright (C) 2018-2018 Oak Ridge National Laboratory (UT-Battelle)
 
-LICENSE: GNU Lesser General Public License v.3
+LICENSE: BSD-3
 
 Persistent location:
 https://github.com/DmitryLyakh/CUDA_Tutorial.git
--- CUDA/BLA_Lib_CUDA/memory.hpp
+++ CUDA/BLA_Lib_CUDA/memory.hpp
@@ -3,20 +3,7 @@
 !Copyright (C) 2018-2018 Dmitry I. Lyakh (Liakh)
 !Copyright (C) 2018-2018 Oak Ridge National Laboratory (UT-Battelle)
 
-!This file is part of CUDA BLA tutorial.
-
-!CUDA BLA is free software: you can redistribute it and/or modify
-!it under the terms of the GNU Lesser General Public License as published
-!by the Free Software Foundation, either version 3 of the License, or
-!(at your option) any later version.
-
-!CUDA BLA is distributed in the hope that it will be useful,
-!but WITHOUT ANY WARRANTY; without even the implied warranty of
-!MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-!GNU Lesser General Public License for more details.
-
-!You should have received a copy of the GNU Lesser General Public License
-!along with CUDA BLA. If not, see <http://www.gnu.org/licenses/>. */
+!This file is part of CUDA BLA tutorial. */
 
 #ifndef _MEMORY_HPP
 #define _MEMORY_HPP
